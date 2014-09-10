ef189f294c25ba4a7c88c2f965ac0698248f7e5c
ragalayaswara <prathima.u@gmail.com>
2014-09-10 12:13:38 -0400

    Update SymbolTable.java

--- SymbolTable.java
+++ SymbolTable.java
@@ -10,25 +10,10 @@ public class SymbolTable{
            symtab.add(s);
            return true; 
 	}
-	/*public boolean lookup(String s)
-	{
-	  int ind;
-	 boolean ps=false;  
-	 for (ind=0;ind<=symtab.size();ind++)
-	  {
-		String element=symtab.get(ind);
-		if(element.equals(s))
-		 return ps=false;
-		else
-		 return ps=true;
-
-	  }
- 	return ps;
-	}*/
 
 	public String toString()
 	{
-	  String res = "---Symbol Table----\n";
+	  String res = "---Symbol Table----\n";   //creating symbol tables ..recognising symbols in the input program
 	  for(int i=0;i<symtab.size();i++)
 	  	res=res+" \n "+symtab.get(i);
 	  System.out.println("\t"+res);
