f9460e6a96af89d06dd8392c6493f1890e0fdca1
ragalayaswara <prathima.u@gmail.com>
2014-09-10 11:58:03 -0400

    Create IfThenElseClause.java

---
+++ IfThenElseClause.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class IfThenElseClause extends Texp implements AST {
+  Texp exp1, exp2; 
+  Tboolexp boolexp;           
+  String kind;                  
+
+  public IfThenElseClause(Tboolexp e1, Texp e2, Texp e3) {
+    Tprogram.LV_Exit_Table_index.remove(Tprogram.counter -1);
+    Tprogram.LV_Exit_Table_index.remove(Tprogram.counter -2);
+    Tprogram.counter=Tprogram.counter-2;
+    
+    Tprogram.counter++;
+    Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter -1).add(Tprogram.counter+1);
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter -1).add(Tprogram.counter+2);
+    boolexp=e1;
+    
+    Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter).add(Tprogram.counter+3);
+    exp1=e2;
+    
+    Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter+1).add(Tprogram.counter+3);
+    exp2=e3;
+    Tprogram.counter=Tprogram.counter+2;
+  }
+
+  public String toString() {
+    return "if "+boolexp+" then "+exp1+" else "+exp2;
+  }
+
+}
