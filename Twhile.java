68ecb2cb5690b9add504ddcdcc90aa7b33834cca
ragalayaswara <prathima.u@gmail.com>
2014-09-30 09:48:03 -0400

    Update Twhile.java

--- Twhile.java
+++ Twhile.java
@@ -8,18 +8,18 @@ class Twhile extends Texp implements AST {
   String kind;                  
 
   public Twhile(Tboolexp e1, Texp e2) {
-    /*Tprogram.LV_Exit_Table_index.remove(Tprogram.counter -1);
+    Tprogram.LV_Exit_Table_index.remove(Tprogram.counter -1);
     Tprogram.LV_Exit_Table_index.remove(Tprogram.counter -2);
     Tprogram.counter=Tprogram.counter-2;
     
     Tprogram.counter++;
     Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
     Tprogram.LV_Exit_Table_index.get(Tprogram.counter -1).add(Tprogram.counter+1);
-    Tprogram.LV_Exit_Table_index.get(Tprogram.counter -1).add(Tprogram.counter+2);*/
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter -1).add(Tprogram.counter+2);
     boolexp=e1;
     
-    /*Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
-    Tprogram.LV_Exit_Table_index.get(Tprogram.counter).add(Tprogram.counter+3);*/
+    Tprogram.LV_Exit_Table_index.add (new ArrayList<Integer>());
+    Tprogram.LV_Exit_Table_index.get(Tprogram.counter).add(Tprogram.counter+3);
     exp1=e2;
     
   }
